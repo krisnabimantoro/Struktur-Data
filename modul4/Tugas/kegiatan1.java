a7344278a5c28ddc263c5b489c801caeb2e3e02d
krisnabimantoro <krisnabmntr@gmail.com>
2024-04-13 17:41:44 +0700

    updated

---
+++ modul4/Tugas/kegiatan1.java
@@ -0,0 +1,25 @@
+package Praktikum.modul4.Tugas;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class kegiatan1 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> candidates = new HashMap<>();
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+    }
+
+    public void calonKandidat() {
+        System.out.println("Pilih kandidat yang ingin Anda dukung: ");
+        System.out.println("- Kandidat B");      
+        System.out.println("- Kandidat C");
+        System.out.println("- Kandidat A");
+
+
+    }
+}
